ab8128383e26af6dee20bd9563dc0234312447f7
bhavantik <gondaliyabhavantik@gmail.com>
2026-04-18 13:18:41 +0530

    sitemap.xml added

---
+++ web/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://distillprep.com',
+  generateRobotsTxt: true, // also creates robots.txt
+  sitemapSize: 7000,
+}
